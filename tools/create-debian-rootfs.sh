c76c800e4eb7c09070d2a9f9bad46427762f16fb
Nate Drude <nate.d@variscite.com>
2024-12-12 18:26:04 +0000

    tools: create-debian-rootfs: Add comments to clarify buid environment
    Add comments to the script to clarify the environment the script has been
    tested in.
    
    Signed-off-by: Nate Drude <nate.d@variscite.com>

--- tools/create-debian-rootfs.sh
+++ tools/create-debian-rootfs.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Description:
+# This script creates a minimal Debian root filesystem tarball using debootstrap.
+# It has been tested in a Multipass Ubuntu 22.04 environment with the following steps:
+# 1. Launch a Multipass instance:
+#    $ multipass launch 22.04 --name ubuntu22-debootstrap --cpus 16 --memory 16G --disk 200G
+# 2. Access the instance:
+#    $ multipass shell ubuntu22-debootstrap
+# 3. Install required dependencies:
+#    $ sudo apt update && sudo apt install qemu-user-static binfmt-support debootstrap zstd tar -y
+# 4. Run this script inside the Multipass instance. The script completes successfully with these dependencies.
+
 # Set variables
 ARCH=arm64
 RELEASE=bookworm
